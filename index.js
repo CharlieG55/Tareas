4df86b5f752a5ac0265eaac0b28e6e68979bb1ab
CharlieG55 <carlosgrajedaf04@gmail.com>
2020-02-09 18:38:20 -0600

    La tarea de semana 3 es subir la tarea de semana 2 a git

---
+++ index.js
@@ -0,0 +1,11 @@
+var mes = 10;
+if (mes == 12 || (mes > 0 && mes <= 2)){
+document.write("Es Invierno");
+} else if (mes >= 3 && mes <= 5) {
+document.write("Es Primavera");
+} else if (mes >= 6 && mes <= 8) {
+document.write("Es Verano");
+} else {
+document.write("Es Otoño");
+};
+document.write("<br>", "Carlos Grajeda - Carne: 20006542");
